03df70aaf67b9876145c985cf1633a2b256e198a
Sergi Mauri Curto <sergimauri8@gmail.com>
2016-09-30 17:32:48 +0200

    Proves Java

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 //Asincron
 
-hello = document.getElementById('hello');
+var hello = document.getElementById('hello');
 hello.style.display = "name";
 hello.innerHTML('hola');
 console.log(hello);
