fec980c7d708910c1611f556f51ccb6343085ca8
Sergi Mauri Curto <sergimauri8@gmail.com>
2016-10-05 20:00:37 +0200

    proves java

--- index.js
+++ index.js
@@ -6,3 +6,4 @@ var hello = document.getElementById('hello');
 hello.style.display = "name";
 hello.innerHTML('hola');
 console.log(hello);
+
---
+++ object.js
@@ -0,0 +1,9 @@
+
+var persona = function() {
+    console.log('Creant una nova persona');
+    this.name='Sergi';
+    this.sn1='Mauri';
+}
+console.log(persona.name);
+//var persona1 = new Persona();
+//console.log(persona1);
